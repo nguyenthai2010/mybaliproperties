46e4774fb7aab61b84e69878681c2d0c7e94c82a
nguyenthai2010 <nguyenthai2010@gmail.com>
2015-08-05 13:27:10 +0700

    .

---
+++ wp-content/themes/mybaliproperties/functions.php
@@ -0,0 +1,17 @@
+<?php
+//add theme support
+add_theme_support('post-thumbnails',array('post'));
+
+
+// Remove Open Sans that WP adds from frontend
+if (!function_exists('remove_wp_open_sans')) :
+    function remove_wp_open_sans() {
+        wp_deregister_style( 'open-sans' );
+        wp_register_style( 'open-sans', false );
+    }
+    add_action('wp_enqueue_scripts', 'remove_wp_open_sans');
+
+    // Uncomment below to remove from admin
+    // add_action('admin_enqueue_scripts', 'remove_wp_open_sans');
+endif;
+
